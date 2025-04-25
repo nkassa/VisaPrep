367c742caa479f0fd329ab8d26907c27c1d0eaf1
natan kassa <nkassa@usc.edu>
2025-04-25 15:09:18 -0700

    new problem

--- sortIntegersByTheNumberOf1Bits.cpp
+++ sortIntegersByTheNumberOf1Bits.cpp
@@ -5,14 +5,8 @@ public:
         priority_queue<pair<int,int>> heap;
         for(int num: arr)
         {
-            int cnt = 0;
-            int val = num;
-            while(num > 0)
-            {
-                cnt++;
-                num &= (num-1);
-            }
-            heap.push({cnt, val});
+            int cnt = __builtin_popcount(num);
+            heap.push({cnt, num});
         }
         vector<int> ans(heap.size(), 0);
         while(heap.size() > 0)
