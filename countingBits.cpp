6bd6b6440b540c8821f2b19b98ae9357f7b7973e
natan kassa <nkassa@usc.edu>
2025-04-26 00:37:55 -0700

    new problem

--- countingBits.cpp
+++ countingBits.cpp
@@ -3,9 +3,9 @@ public:
     vector<int> countBits(int n) 
     {
         vector<int> ans(n+1, 0);
-        for(int i = 0; i <= n; i++)
+        for(int i = 1; i <= n; i++)
         {
-            ans[i] = __builtin_popcount(i);
+            ans[i] = ans[i & i-1] + 1;
         }
         return ans;
     }
