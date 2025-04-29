04b8d9e71566dbb46ed86e5e3dd1bcf1b532ce03
natan kassa <nkassa@usc.edu>
2025-04-29 12:55:08 -0700

    new problem

--- searchA2DMatrix.cpp
+++ searchA2DMatrix.cpp
@@ -4,26 +4,20 @@ public:
     {
         int m = matrix.size();
         int n = matrix[0].size();
-        vector<vector<int>> list;
-        for(int row = 0; row < m; row++)
-        {
-            for(int col = 0; col < n; col++)
-            {
-                list.push_back(matrix[row][col]);
-            }
-        }
         int left = 0;
-        int right = list.size()-1;
+        int right = (m*n)-1;
         while(left < right)
         {
             int mid = left + (right-left)/2;
-            if(list[mid] == target)
+            int row = mid / m;
+            int col = mid % n;
+            if(matrix[row][col] == target)
             {
                 return true;
             }
-            if(list[mid] > target)
+            if(matrix[row][col] > target)
             {
-                right = mid;
+                right = mid-1;
             }
             else
             {
