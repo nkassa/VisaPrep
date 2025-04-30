da80b49250c2ebaf869b26b57a150f3e15e5089a
natan kassa <nkassa@usc.edu>
2025-04-30 13:15:52 -0700

    new problem

--- longestCommonPrefix.cpp
+++ longestCommonPrefix.cpp
@@ -1,20 +1,17 @@
 class Solution {
 public:
-    string longestCommonPrefix(vector<string>& strs) {
-        if(strs.size() == 0)
-        {
-            return "";
-        }
+    string longestCommonPrefix(vector<string>& strs) 
+    {
         string prefix = strs[0];
-        for(int i = 1; i < strs.size(); i++)
+        for(string str: strs)
         {
-            while(strs[i].find(prefix) != 0)
+            while(prefix.size() > 0 && str.find(prefix) != 0)
             {
-                prefix = prefix.substr(0, prefix.length()-1);
-                if(prefix.empty())
-                {
-                    return "";
-                }
+                prefix.pop_back();
+            }
+            if(prefix.size() == 0)
+            {
+                return "";
             }
         }
         return prefix;
