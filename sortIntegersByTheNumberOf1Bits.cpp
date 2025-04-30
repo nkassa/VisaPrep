793a2ccf0409ac7a3ac95e0749ec7931eacede10
natan kassa <nkassa@usc.edu>
2025-04-30 13:22:42 -0700

    new problem

--- sortIntegersByTheNumberOf1Bits.cpp
+++ sortIntegersByTheNumberOf1Bits.cpp
@@ -2,10 +2,17 @@ class Solution {
 public:
     vector<int> sortByBits(vector<int>& arr) 
     {
-        priority_queue< pair<int,int>,  vector<pair<int,int>>, greater<pair<int,int>>> heap;
+        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> heap;
         for(int num: arr)
         {
-            heap.push({__builtin_popcount(num), num});
+            int cnt = 0;
+            int val = num;
+            while(num > 0)
+            {
+                cnt += 1;
+                num = num & (num-1);
+            }
+            heap.push({cnt, val});
         }
         vector<int> ans;
         while(!heap.empty())
