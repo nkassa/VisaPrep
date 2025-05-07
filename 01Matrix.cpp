df1413a8456340831da88e30dc97184bb0550270
natan kassa <nkassa@usc.edu>
2025-05-06 22:52:07 -0700

    new problem

--- 01Matrix.cpp
+++ 01Matrix.cpp
@@ -1,20 +1,22 @@
 class Solution {
 public:
+    vector<vector<int>> directions = {{0,1}, {1,0}, {0,-1}, {-1,0}};
     int m;
     int n;
+    vector<vector<int>> mat;
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) 
     {
+        this->mat = mat;
+        queue<vector<int>> queue;
         m = mat.size();
         n = mat[0].size();
-        vector<vector<int>> directions = {{0,1}, {1,0}, {0,-1}, {-1,0}};
-        queue<pair<int,int>> queue;
         for(int row = 0; row < m; row++)
         {
             for(int col = 0; col < n; col++)
             {
                 if(mat[row][col] == 0)
                 {
-                    queue.push({row,col});
+                    queue.push({row,col, 1});
                 }
                 else
                 {
@@ -24,17 +26,18 @@ public:
         }
         while(!queue.empty())
         {
-            int row = queue.front().first;
-            int col = queue.front().second;
+            int row = queue.front()[0];
+            int col = queue.front()[1];
+            int steps = queue.front()[2];
             queue.pop();
             for(vector<int> direction: directions)
             {
-                int nextRow = row + direction[0];
-                int nextCol = col + direction[1];
-                if(valid(nextRow, nextCol) && mat[nextRow][nextCol] > mat[row][col] + 1)
+                int nextRow = direction[0] + row;
+                int nextCol = direction[1] + col;
+                if(valid(nextRow, nextCol) && steps  < mat[nextRow][nextCol])
                 {
-                    mat[nextRow][nextCol] = mat[row][col] + 1;
-                    queue.push({nextRow,nextCol});
+                    mat[nextRow][nextCol] = steps;
+                    queue.push({nextRow, nextCol, steps + 1});
                 }
             }
         }
@@ -42,6 +45,6 @@ public:
     }
     bool valid(int row, int col)
     {
-        return row >= 0 && row < m && col >= 0 && col < n;
+        return 0 <= row && row < m && 0 <= col && col < n && mat[row][col] != 0;
     }
 };
