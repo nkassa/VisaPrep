11acaf2cac5f3c7edd3486c5ad384fbde892d8fb
natan kassa <nkassa@usc.edu>
2025-05-09 16:59:53 -0700

    new problem

--- subarraySumEqualsK.cpp
+++ subarraySumEqualsK.cpp
@@ -2,22 +2,21 @@ class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) 
     {
-        int left = 0;
         int ans = 0;
         int sum = 0;
-        for(int right = 0; right < nums.size(); right++)
+        unordered_map<int,int> seen;
+        seen[0] = 1;
+        for(int num: nums)
         {
-            sum += nums[right];
-            while(sum >= k && left <= right)
+            sum += num;
+            int diff = sum - k;
+            if(seen.find(diff) != seen.end())
             {
-                if(sum == k)
-                {
-                    ans++;
-                }
-                sum -= nums[left];
-                left++;
+                ans += seen[diff];
             }
+            seen[sum]++;
         }
         return ans;
     }
 };
+
