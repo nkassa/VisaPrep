dd92edf74606e5daf24f3bc23e4ff885d94d2586
natan kassa <nkassa@usc.edu>
2025-05-09 23:40:55 -0700

    new problem

--- searchA2DMatrix.cpp
+++ searchA2DMatrix.cpp
@@ -5,7 +5,7 @@ public:
         int m = matrix.size();
         int n = matrix[0].size();
         int left = 0;
-        int right = (m*n)-1;
+        int right = (m*n) - 1;
         while(left <= right)
         {
             int mid = left + (right - left) / 2;
@@ -17,13 +17,14 @@ public:
             }
             else if(matrix[row][col] < target)
             {
-                left = mid+1;
+                left = mid + 1;
             }
             else
             {
-                right = mid-1;
+                right = mid - 1;
             }
         }
         return false;
     }
 };
+// practice
