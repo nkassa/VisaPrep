2d9bfdd394a3a91eb398e5238c9be3b04ef15aff
natan kassa <nkassa@usc.edu>
2025-05-10 00:47:58 -0700

    new problem

---
+++ indexesOfSubarraySum.cpp
@@ -0,0 +1,63 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    vector<int> subarraySum(vector<int> &arr, int target) 
+    {
+        // code here
+        unordered_map<int,int> seen;
+        seen[0] = 0;
+        int sum = 0;
+        vector<int> ans; 
+        for(int i = 0; i < arr.size(); i++)
+        {
+            sum += arr[i];
+            int diff = sum - target;
+            if(seen.find(diff) != seen.end())
+            {
+                return {seen[diff], i};
+            }
+            seen[sum] = i;
+        }
+        return {-1};
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        int d;
+        string input;
+
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        cin >> d;
+        cin.ignore();
+
+        Solution ob;
+        vector<int> result = ob.subarraySum(arr, d);
+        for (int i : result) {
+            cout << i << " ";
+        }
+        cout << "\n~\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
