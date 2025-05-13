ca546a457c677701b9e6a28af68b0ef3e8f3bc56
natan kassa <nkassa@usc.edu>
2025-05-12 17:28:05 -0700

    new problem

--- peakElement.cpp
+++ peakElement.cpp
@@ -2,32 +2,29 @@ class Solution {
   public:
     int peakElement(vector<int> &arr) 
     {
+        
         // Your code here
-        for(int i = 0; i < arr.size(); i++)
+        if(arr.size() == 1)
+        {
+            return 0;
+        }
+        for(int i = 1; i < arr.size()-1; i++)
         {
             long long int left = arr[i] - arr[i-1];
             long long int right = arr[i] - arr[i+1];
-            if(i == 0)
-            {
-                left = 0;
-                if(right > 0)
-                {
-                    return i;
-                }
-            }
-            if(i == arr.size()-1)
-            {
-                right = 0;
-                if(left > 0)
-                {
-                    return i;
-                }
-            }
-            if(left > 0 && right > 0)
+            if(left >= 0 && right >= 0)
             {
                 return i;
             }
         }
-        return 0;
+        if(arr[0] > arr[1])
+        {
+            return 0;
+        }
+        if(arr[arr.size()-1] > arr[arr.size()-2] )
+        {
+            return arr.size()-1;
+        }
+        return -1;
     }
 };
