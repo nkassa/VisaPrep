2657aab4c497e0af8da814c6953c793c7a0ff2b2
natan kassa <nkassa@usc.edu>
2025-05-16 23:53:15 -0700

    new problem

--- longestPlaindromicSubstring.cpp
+++ longestPlaindromicSubstring.cpp
@@ -1,8 +1,9 @@
 class Solution {
   public:
-    int ans = 0;
     string longestPalindrome(string s) 
     {
+        string ans = "";
+        int size = 0;
         // code here
         for(int i = 0; i < s.size(); i++)
         {
@@ -12,4 +13,18 @@ class Solution {
         return ans;
     }
     void helper(int left, int right, string& s)
+    {
+        int cnt = 0;
+        while(left >= 0 && right < s.size() && s[left] == s[right])
+        {
+            left++;
+            right--;
+            cnt++;
+        }
+        if(size < cnt)
+        {
+            size = cnt;
+            ans = s.sbstr(left, right-left);
+        }
+    }
 };
