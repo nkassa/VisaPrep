8711d75c841f59de85cf12fff0db251896811a91
natan kassa <nkassa@usc.edu>
2025-05-25 00:00:26 -0700

    new problem

---
+++ minimumAbsoluteDifferenceInBST.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> list;
+    int getMinimumDifference(TreeNode* root) 
+    {
+        dfs(root);
+        sort(list.begin(), list.end());
+        int diff = INT_MAX;
+        for(int i = 1; i < list.size(); i++)
+        {
+            diff = min(diff,abs(list[i] - list[i-1]));
+        }
+        return diff;
+    }
+    void dfs(TreeNode* root)
+    {
+        if(!root)
+        {
+            return;
+        }
+        list.push_back(root->val);
+        dfs(root->left);
+        dfs(root->right);
+    }
+};
